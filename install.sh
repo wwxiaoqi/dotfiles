519db093759c7a9664ef29d618bda97503984bfb
wwxiaoqi <tahicoing@gmail.com>
2023-01-28 17:31:32 +0800

    feat: add stow installer script

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+usage() {
+    printf "\
+        Usage:
+    $0 [name]
+    $0 all
+    "
+}
+
+if [[ -n "$1" ]]; then
+    if [[ "$1" == "all" ]]; then
+        all="alacritty bspwm dunst fish scripts"
+    else
+        all=$*
+    fi
+else
+    usage
+    exit 1
+fi
+
+for i in $all; do
+    echo "Installing: $i"
+    stow -t $HOME $i || exit -1
+done
+
+echo "Installation Success!"
