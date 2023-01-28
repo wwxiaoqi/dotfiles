49f2237791af5a9b27700a60979e8234c905caaf
wwxiaoqi <tahicoing@gmail.com>
2023-01-28 17:14:22 +0800

    feat: add bspwm configuration

---
+++ scripts/polybar-launch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+killall -q polybar
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload one &
+done
